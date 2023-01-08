4fbdec8d47755d5432c81947e5c9747832750e55
MIYABETaisei <mytspe46@gmail.com>
2023-01-08 12:53:32 +0900

    ✨ how coding of ref has types

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,51 @@
 <template>
   <div class="app">
-    <p>{{ name }} - {{ age }}</p>
-    <button @click="changeName('Zelda')">change name</button>
-    <button @click="changeAge(30)">change age</button>
+    <p>{{ jobs[0].location }}</p>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, toRefs, ref } from "vue";
+import { defineComponent, ref } from "vue";
+import Job from "./types/Job";
 
 export default defineComponent({
   name: "App",
   components: {},
   setup() {
-    // const state = reactive({
-    //   name: "Link",
-    //   age: 25 as number,
-    // });
+    const jobs = ref<Job[]>([
+      {
+        title: "farm worker",
+        location: "lon lon ranch",
+        salary: 30000,
+        id: "1",
+      },
+      {
+        title: "quarryman",
+        location: "death mountain",
+        salary: 40000,
+        id: "2",
+      },
+      {
+        title: "flute player",
+        location: "the lost woods",
+        salary: 35000,
+        id: "3",
+      },
+      {
+        title: "fisherman",
+        location: "lake hylia",
+        salary: 21000,
+        id: "4",
+      },
+      {
+        title: "prison guard",
+        location: "gerudo valley",
+        salary: 32000,
+        id: "5",
+      },
+    ]);
 
-    // state.name = 999 // cannnot change type
-
-    // return {
-    //   ...toRefs(state),
-    // };
-
-    const name = ref("Link");
-    const age = ref<number>(25);
-
-    return {
-      name,
-      age,
-    };
-  },
-  methods: {
-    changeName(name: string) {
-      this.name = name;
-      return name;
-    },
-    changeAge(age: number) {
-      this.age = age;
-      return age;
-    },
+    return { jobs };
   },
 });
 </script>
